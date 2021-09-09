62e3771ed5dc5e8fbf76268733b92c0dd89b0615
Andres Villaquiran <72285096+andrsvilla@users.noreply.github.com>
2021-09-09 16:49:22 -0500

    Delete WriteMultipleFields directory

--- WriteMultipleFields/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "FL_VILLA"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "123villa789"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 1373623			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "5YDH3X1BYS1X1AFI"   // replace XYZ with your channel write API Key
